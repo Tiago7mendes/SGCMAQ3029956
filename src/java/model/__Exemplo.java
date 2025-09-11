d8b86bcac726fc2e57f32823d388be944857b695
Tiago7mendes <tiagosettimendes@gmail.com>
2025-09-11 11:28:23 -0300

    adding controller and building model

--- src/java/model/__Exemplo.java
+++ src/java/model/__Exemplo.java
@@ -1,17 +1,30 @@
 package model;
 
+import java.sql.SQLException;
+
 public class __Exemplo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
         TipoUsuario tp = new TipoUsuario();
         
-        tp.setId(5);
-        tp.setModuloAdministrativo("N");
-        tp.setModuloAgendamento("N");
-        tp.setModuloAtendimento("S");
-        
-        tp.save(); // insert
-        
-        tp.setModuloAdministrativo("S");
-        tp.save(); // update
+//        tp.setId(7);
+//        tp.setModuloAdministrativo("N");
+//        tp.setModuloAgendamento("N");
+//        tp.setModuloAtendimento("S");
+//        
+//        tp.save(); // insert
+//        
+//        tp.setModuloAdministrativo("S");
+//        tp.save(); // update
+
+          tp.setId(7);
+          boolean status = tp.load(); // select (read)
+          System.out.println(status);
+          System.out.println(tp);
+          
+          tp.setNome("tipo 7");
+          tp.save(); // update
+          
+//          tp.delete();
     }
 }
