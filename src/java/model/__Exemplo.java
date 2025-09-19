1a8f698e02cad8c48881a0fe5548d8c690caa760
Tiago7mendes <tiagosettimendes@gmail.com>
2025-09-19 10:43:06 -0300

    fixing servlet and the form

--- src/java/model/__Exemplo.java
+++ src/java/model/__Exemplo.java
@@ -6,24 +6,24 @@ import java.util.ArrayList;
 public class __Exemplo {
     public static void main(String[] args) throws SQLException {
 
-        TipoUsuario tp = new TipoUsuario();
-        
-        tp.setId(12);
-        tp.setModuloAdministrativo("N");
-        tp.setModuloAgendamento("N");
-        tp.setModuloAtendimento("S");
-        
-        tp.save(); // insert
-        
-        tp.setModuloAdministrativo("S");
-        tp.save(); // update
-
-//          tp.setId(7);
+//        TipoUsuario tp = new TipoUsuario();
+////        
+////        tp.setId(12);
+////        tp.setModuloAdministrativo("N");
+////        tp.setModuloAgendamento("N");
+////        tp.setModuloAtendimento("S");
+////        
+////        tp.save(); // insert
+////        
+////        tp.setModuloAdministrativo("S");
+////        tp.save(); // update
+//
+//          tp.setId(12);
 //          boolean status = tp.load(); // select (read)
 //          System.out.println(status);
 //          System.out.println(tp);
 //          
-//          tp.setNome("tipo 7");
+//          tp.setNome("tipo 12");
 //          tp.save(); // update
 //          
 ////          tp.delete();
@@ -31,16 +31,16 @@ public class __Exemplo {
 //          ArrayList<TipoUsuario> list = new TipoUsuario().getAllTableEntities();
 //          System.out.println(list);
           
-//         Usuario us = new Usuario();
-//         
-//         us.setId(12);
-//         us.setNome("Carlinhos");
-//         us.setCpf("12342355476");
-//         us.setSenha("asbdjk");
-//         us.setTipoUsuarioId(5);
-//         us.save();
+         Usuario us = new Usuario();
+         
+         us.setId(12);
+         us.setNome("Carlinhos");
+         us.setCpf("12342355476");
+         us.setSenha("asbdjk");
+         us.setTipoUsuarioId(5);
+         us.save();
 
-//          Usuario us2 = new Usuario();
+          Usuario us2 = new Usuario();
           
     }
 }
